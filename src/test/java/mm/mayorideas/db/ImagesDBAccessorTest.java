3be8957180a433a2c326c1ed8e6f65a46cb80248
MarekMatejkaKCL <marek.matejka@kcl.ac.uk>
2016-04-14 23:47:49 +0200

    adding an extra test to ImageDBAccessor

--- src/test/java/mm/mayorideas/db/ImagesDBAccessorTest.java
+++ src/test/java/mm/mayorideas/db/ImagesDBAccessorTest.java
@@ -2,11 +2,21 @@ package mm.mayorideas.db;
 
 import org.junit.Test;
 
+import java.io.File;
 import java.util.List;
 
 import static org.junit.Assert.*;
 
 public class ImagesDBAccessorTest {
+
+    @Test
+    public void testGetImage() throws Exception {
+        ImagesDBAccessor db = ImagesDBAccessor.getInstance();
+        final File image = db.getImage(1);
+        assertTrue(image.isFile());
+        assertTrue(image.getTotalSpace() > 0);
+        image.delete();
+    }
 
     @Test
     public void testGetImageIdsForIdea() throws Exception {
